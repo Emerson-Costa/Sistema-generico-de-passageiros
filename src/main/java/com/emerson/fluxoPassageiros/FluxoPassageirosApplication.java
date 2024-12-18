475fc8f2fb23be92160746dc98926502c66a51fe
Emerson Costa <emersonjscosta@gmail.com>
2024-12-18 13:15:31 -0300

    Projeto Iniciado

---
+++ src/main/java/com/emerson/fluxoPassageiros/FluxoPassageirosApplication.java
@@ -0,0 +1,13 @@
+package com.emerson.fluxoPassageiros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxoPassageirosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FluxoPassageirosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/emerson/fluxoPassageiros/FluxoPassageirosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emerson.fluxoPassageiros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxoPassageirosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
